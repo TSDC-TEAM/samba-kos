d14039e67c05e40ffb5bffe30b3356ff5b7ce3c6
imarochkin <imarochkin@octopus.name>
2022-01-04 10:42:23 +0300

    OPENSSL: cipher init, deinit and encrypt impl

--- kos/libs/gnutls/kosgnutls_cipher.c
+++ kos/libs/gnutls/kosgnutls_cipher.c
@@ -2,6 +2,8 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <memory.h>
+#include <openssl/crypto.h>
+#include <openssl/evp.h>
 #include "kosgnutls.h"
 
 #define GNUTLS_CIPHER_AES_192_GCM GNUTLS_CIPHER_AES_128_GCM
@@ -968,6 +970,32 @@ int gnutls_cipher_init(gnutls_cipher_hd_t * handle,
                        gnutls_cipher_algorithm_t cipher,
                        const gnutls_datum_t * key, const gnutls_datum_t * iv)
 {
+    if (cipher != GNUTLS_CIPHER_ARCFOUR_128) {
+        fprintf(stderr, "CIPHER unknown\n");
+        exit(1);
+    }
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX_init(ctx);
+
+    int res_init = EVP_EncryptInit_ex(ctx, EVP_rc4(), NULL, NULL, NULL);
+    if (!res_init) {
+        fprintf(stderr, "CIPHER Init 1 failed\n");
+        exit(1);
+    }
+
+    EVP_CIPHER_CTX_set_key_length(ctx, (int)key->size);
+
+    res_init = EVP_EncryptInit_ex(ctx, NULL, NULL, (const unsigned char *)key, NULL);
+    if (!res_init) {
+        fprintf(stderr, "CIPHER Init 2 failed\n");
+        exit(1);
+    }
+
+    *handle = ctx;
+
+    return 0;
+
     api_cipher_hd_st *h;
     int ret;
     const cipher_entry_st* e;
@@ -1013,11 +1041,6 @@ int gnutls_cipher_init(gnutls_cipher_hd_t * handle,
     return ret;
 }
 
-//void gnutls_cipher_deinit(gnutls_cipher_hd_t handle)
-//{
-//    (void)handle;
-//}
-
 #define GNUTLS_CIPHER_LOOP(b) \
 	const cipher_entry_st *p; \
 		for(p = algorithms; p->name != NULL; p++) { b ; }
@@ -1036,13 +1059,46 @@ const cipher_entry_st *_gnutls_cipher_to_entry(gnutls_cipher_algorithm_t c)
 
 void gnutls_cipher_deinit(gnutls_cipher_hd_t handle)
 {
-    (void)handle;
+    unsigned char out[1028] = {0};
+    int outl;
+
+    EVP_CIPHER_CTX *ctx = handle;
+    int res_fin = EVP_EncryptFinal_ex(ctx, out, &outl);
+    if (!res_fin) {
+        fprintf(stderr, "Fin failed\n");
+        exit(1);
+    }
+
+    if (0 != outl) {
+        fprintf(stderr, "CIPHER fin failed\n");
+        exit(1);
+    }
+
+    EVP_CIPHER_CTX_free(ctx);
 }
 
 int gnutls_cipher_encrypt(gnutls_cipher_hd_t handle, void *ptext,
                           size_t ptext_len)
 {
-    return -1;
+    DumpHex(ptext, ptext_len);
+    EVP_CIPHER_CTX *ctx = handle;
+
+    unsigned char out[1028] = {0};
+    int outl;
+    int res_update = EVP_EncryptUpdate(ctx, out, &outl, ptext, (int)ptext_len);
+    if (!res_update) {
+        fprintf(stderr, "CIPHER Update failed\n");
+        exit(1);
+    }
+
+    if (outl != ptext_len) {
+        fprintf(stderr, "CIPHER res len failed\n");
+        exit(1);
+    }
+
+    memcpy(ptext, out, outl);
+
+    return 0;
 }
 
 int gnutls_cipher_decrypt(gnutls_cipher_hd_t handle, void *ctext,
