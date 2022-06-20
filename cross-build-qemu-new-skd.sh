fb384f5e276d371df5e6a9c9572f4853738bba58
imarochkin <imarochkin@octopus.name>
2022-06-20 11:01:54 +0300

    SKD 238 build script

---
+++ cross-build-qemu-new-skd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BUILD=$PWD/build-qemu-new-sdk
+mkdir -p $BUILD && cd $BUILD
+
+export LANG=C
+export TARGET="aarch64-kos"
+export PKG_CONFIG=""
+export SDK_PREFIX="/opt/KasperskyOS-Community-Edition-1.1.0.238"
+export INSTALL_PREFIX=$BUILD/../install
+BUILD_SIM_TARGET="y"
+export PATH="$SDK_PREFIX/toolchain/bin:$PATH"
+
+cmake -G "Unix Makefiles" \
+      -D CMAKE_BUILD_TYPE:STRING=Debug \
+      -D CMAKE_INSTALL_PREFIX:STRING=$BUILD/../install \
+      -D CMAKE_TOOLCHAIN_FILE=$SDK_PREFIX/toolchain/share/toolchain-${TARGET}.cmake \
+      ../ && make -j$(nproc) sim
