cebacc8feb0c566d102853f955e1f9588ddd3221
imarochkin <imarochkin@octopus.name>
2022-07-11 14:30:53 +0300

    LINUX: revert testing build to prod

--- kos_create_share_and_conf.py
+++ kos_create_share_and_conf.py
@@ -3,7 +3,10 @@
 import sys, getopt, os, errno
 
 def main(argv):
-    privateDirName = "private"
+    passdbFile = os.getcwd() + "/kos/resources/ramfs/usr/local/samba/private/passdb.tdb"
+    print(passdbFile)
+
+    privateDirName = "/private"
 
     folderName = ''
     try:
@@ -13,6 +16,8 @@ def main(argv):
     for opt, arg in opts:
         if opt in ("-f", "--folder"):
             folderName = arg
+
+    privateDirName = folderName.rpartition("/")[0] + privateDirName
 
     try:
         os.mkdir(folderName)
@@ -26,13 +31,16 @@ def main(argv):
 
     os.chmod(folderName, 0o777)
     os.chmod(folderName + "2", 0o777)
-    os.chmod(privateDirName, 0o666)
+    os.chmod(privateDirName, 0o755)
+
+    os.system("cp " + passdbFile + " " + privateDirName)  
 
     try:
         contName = folderName.rpartition("/")[0] + "/smb.conf"
         folderName
         conf = open(contName, "w")
         confText = ''
+
         confText += "[global]\n"
         confText += "map to guest = bad user\n"
         confText += "workgroup = WORKGROUP\n"
@@ -43,17 +51,31 @@ def main(argv):
         confText += "dns proxy = no\n"
         confText += "disable netbios = yes\n"
         confText += "smb ports = 1490\n"
-        confText += "log level = 10\n"
-        confText += "lock directory = ./\n"
+        confText += "log level = 5\n"
+        confText += "lock directory = " + privateDirName + "\n"
+        confText += "private dir = " + privateDirName + "\n"
         confText += "restrict anonymous = 2\n"
-        confText += "\n"
-        confText += "[test]\n"
+        confText += "valid users = @user, @root\n"
+        confText += "server min protocol = SMB2_10\n"
+        confText += "server max protocol = SMB2_10\n"
+
+        confText += "[tmp]\n"
         confText += "path=" + folderName + "\n"
         confText += "public = yes\n"
         confText += "browseable = yes\n"
         confText += "writable = yes\n"
         confText += "guest ok = no\n"
         confText += "read only = no\n"
+
+        confText += "[tmp2]\n"
+        confText += "path=" + folderName + "2\n"
+        confText += "valid users = user, root\n"
+        confText += "browseable = yes\n"
+        confText += "guest ok = no\n"
+        confText += "read only = yes\n"
+        confText += "read list = user, root\n"
+        confText += "write list = user\n"
+
         conf.write(confText)
         conf.close
     except FileExistsError as e:
